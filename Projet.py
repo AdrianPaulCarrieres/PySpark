a3914c6e6b5a2127a2beb1ab83a284b5e79a46a5
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2022-01-17 21:29:13 +0100

    exporting file

--- Projet.py
+++ Projet.py
@@ -4,10 +4,6 @@
 # # Projet
 
 # ## Imports & lecture des fichiers
-
-# In[1]:
-
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
@@ -24,44 +20,22 @@ df1.show()
 
 df2.show()
 
-
 # ## Join the dataframes et conversion
-
-# In[2]:
-
-
 join_express = df1["Currency"] == df2["ISO_4217"]
 joined = df1.join(df2, join_express, "inner")
-
-
-# In[3]:
-
 
 joined_and_converted_prices = joined.withColumn(
     "Dollar Prices", round(expr("Price / Dollar_To_Curr_Ratio"), 2))
 joined_and_converted_prices.show()
 
-
-# In[37]:
-
-
 joined_and_converted_prices = joined_and_converted_prices.drop(
     "ISO_4217").drop("Dollar_To_Curr_Ratio")
 joined_and_converted_prices.show(1000)
 
-
 # # Ecart à la moyenne
-
-# In[30]:
-
-
 avg_prices = joined_and_converted_prices.groupBy("Country").agg(avg(col("Dollar Prices"))).sort("Country").select(
     "Country", round("`avg(Dollar Prices)`", 2)) .withColumnRenamed("round(avg(Dollar Prices), 2)", "Moyenne")
 avg_prices.show(100)
-
-
-# In[38]:
-
 
 usa_average = avg_prices.where(
     col("Country") == "United States").select("Moyenne").collect()[0][0]
@@ -69,28 +43,13 @@ usa_average = str(usa_average)
 avg_prices.withColumn("Ecart à la moyenne (%)", round(expr(
     f"(abs(Moyenne) - {usa_average})/ {usa_average} * 100"), 2)).sort(desc("Ecart à la moyenne (%)")).show(100)
 
-
 # ## Somme totale
-
-# In[44]:
-
-
 joined_and_converted_prices.groupBy("Country").agg({"Dollar Prices": "sum"}).select("Country", round(
     "sum(Dollar Prices)", 2)).withColumnRenamed("round(sum(Dollar Prices), 2)", "Somme des produits").sort(desc("sum(Dollar Prices)")).show(100)
 
-
 # ## Liste des produits
-
-# In[45]:
-
-
 df1.dropDuplicates(["Model_name"]).sort("Model_name").show(1000)
 
-
 # ## Pays le moins cher pour acheter les AirPods Pro
-
-# In[46]:
-
-
 joined_and_converted_prices.filter(
     "Model_name == 'AirPods Pro'").sort(asc("Dollar Prices")).show(1)
